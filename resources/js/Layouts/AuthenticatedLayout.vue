c4140698f94589461533453b7d806a156c39aff5
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-10-23 15:36:23 +0700

    change login page & add logout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,51 +1,59 @@
 <script setup>
 import { ref } from "vue";
 import NavLink from "@/Components/NavLink.vue";
-import { usePage } from "@inertiajs/vue3";
+import { Settings, ChevronDown } from "lucide-vue-next";
+import { router, usePage } from "@inertiajs/vue3";
 
 const { props } = usePage();
 const role = props.auth.user.role;
 const showOrderBesarGroup = ref(false);
 const showOrderKecilGroup = ref(false);
+const showOption = ref(false);
 
 const toggleDropdown = (dropDownId) => {
     if (dropDownId === 1) {
         showOrderBesarGroup.value = !showOrderBesarGroup.value;
         showOrderKecilGroup.value = false;
+        showOption.value = false;
     } else if (dropDownId === 2) {
         showOrderKecilGroup.value = !showOrderKecilGroup.value;
         showOrderBesarGroup.value = false;
+        showOption.value = false;
+    } else if (dropDownId === 3) {
+        showOption.value = !showOption.value;
+        showOrderKecilGroup.value = false;
+        showOrderBesarGroup.value = false;
     }
 }
+
+const logout = () => {router.post(route('logout'))};
 </script>
 <template>
     <div class="h-full min-h-screen bg-gradient-to-b from-slate-50 to-slate-100">
-        <div class="w-full bg-white drop-shadow-md px-8 py-4 flex justify-between gap-4 items-center sticky top-0 z-50">
+        <div class="w-full bg-white shadow-lg px-6 py-4 flex justify-between items-center sticky top-0 z-50 rounded-lg">
             <!-- Logo Peruri -->
             <img :src="'/img/peruri.png'" class="w-24" />
 
             <!-- Navigation -->
-            <div class="flex justify-start items-center gap-6">
+            <div class="flex justify-start items-center gap-8">
                 <!-- Order Besar -->
                 <div class="relative">
-                    <div @click="toggleDropdown(1)" class="flex items-center cursor-pointer">
+                    <div @click="toggleDropdown(1)" class="flex items-center cursor-pointer hover:text-blue-600 transition duration-200">
                         <span class="text-sm font-semibold text-gray-800">Order Besar</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-1">
-                            <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd" />
-                        </svg>
+                        <ChevronDown class="w-5 h-5 ml-1" />
                     </div>
                     <Transition name="fade">
-                        <div v-show="showOrderBesarGroup" class="absolute left-0 mt-2 w-48 bg-white shadow-lg z-10">
+                        <div v-show="showOrderBesarGroup" class="absolute left-0 mt-2 w-48 bg-white shadow-lg z-10 rounded-lg">
                             <NavLink
                                 :href="route('orderBesar.poSiapVerif')"
                                 :active="route().current('orderBesar.poSiapVerif')"
-                                class="block text-gray-700  px-4 py-2 transition duration-200 ease-in-out w-full">
+                                class="block text-gray-700 px-4 py-2 transition duration-200 ease-in-out hover:bg-gray-100 w-full">
                                 Order Siap Periksa
                             </NavLink>
                             <NavLink
                                 :href="route('orderBesar.registerNomorPo')"
                                 :active="route().current('orderBesar.registerNomorPo')"
-                                class="block text-gray-700 px-4 py-2 transition duration-200 ease-in-out w-full">
+                                class="block text-gray-700 px-4 py-2 transition duration-200 ease-in-out hover:bg-gray-100 w-full">
                                 Register Nomor PO
                             </NavLink>
                         </div>
@@ -54,18 +62,16 @@ const toggleDropdown = (dropDownId) => {
 
                 <!-- Order Kecil -->
                 <div class="relative">
-                    <div @click="toggleDropdown(2)" class="flex items-center cursor-pointer">
+                    <div @click="toggleDropdown(2)" class="flex items-center cursor-pointer hover:text-blue-600 transition duration-200">
                         <span class="text-sm font-semibold text-gray-800">Order Kecil</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-1">
-                            <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd" />
-                        </svg>
+                        <ChevronDown class="w-5 h-5 ml-1" />
                     </div>
                     <Transition name="fade">
-                        <div v-show="showOrderKecilGroup" class="absolute left-0 mt-2 w-48 bg-white shadow-lg z-10">
+                        <div v-show="showOrderKecilGroup" class="absolute left-0 mt-2 w-48 bg-white shadow-lg z-10 rounded-lg">
                             <NavLink
                                 :href="route('orderKecil.cetakLabel')"
                                 :active="route().current('orderKecil.cetakLabel')"
-                                class="block text-gray-700  px-4 py-2 transition duration-200 ease-in-out w-full">
+                                class="block text-gray-700 px-4 py-2 transition duration-200 ease-in-out hover:bg-gray-100 w-full">
                                 Cetak Label
                             </NavLink>
                         </div>
@@ -76,7 +82,7 @@ const toggleDropdown = (dropDownId) => {
                 <NavLink
                     :href="route('monitoringProduksi.statusVerif.index')"
                     :active="route().current('monitoringProduksi.statusVerif.index')"
-                    class="text-gray-800  px-2 py-1 transition duration-200 ease-in-out">
+                    class="text-gray-800 px-2 py-1 transition duration-200 ease-in-out hover:text-blue-600">
                     Monitoring Produksi
                 </NavLink>
 
@@ -84,7 +90,7 @@ const toggleDropdown = (dropDownId) => {
                 <NavLink
                     :href="route('dataPo.index',0)"
                     :active="route().current('dataPo.index')"
-                    class="text-gray-800  px-2 py-1 transition duration-200 ease-in-out">
+                    class="text-gray-800 px-2 py-1 transition duration-200 ease-in-out hover:text-blue-600">
                     Data PO
                 </NavLink>
 
@@ -92,7 +98,7 @@ const toggleDropdown = (dropDownId) => {
                 <NavLink
                     :href="route('monitoringProduksi.produksiPegawai')"
                     :active="route().current('monitoringProduksi.produksiPegawai')"
-                    class="text-gray-800 px-2 py-1 transition duration-200 ease-in-out">
+                    class="text-gray-800 px-2 py-1 transition duration-200 ease-in-out hover:text-blue-600">
                     Produksi Pegawai
                 </NavLink>
 
@@ -100,13 +106,27 @@ const toggleDropdown = (dropDownId) => {
                 <NavLink v-if="role === 1"
                     :href="route('createUser.index')"
                     :active="route().current('createUser.index')"
-                    class="text-gray-800  px-2 py-1 transition duration-200 ease-in-out">
+                    class="text-gray-800 px-2 py-1 transition duration-200 ease-in-out hover:text-blue-600">
                     Create User
                 </NavLink>
             </div>
 
             <!-- Option -->
-            <div></div>
+            <div class="relative">
+                <div @click="toggleDropdown(3)" class="flex items-center cursor-pointer p-2 rounded-lg hover:bg-gray-100 transition duration-200 ease-in-out">
+                    <Settings class="w-6 h-6 text-gray-800" />
+                </div>
+                <Transition name="fade">
+                    <div v-show="showOption" class="absolute -left-20 mt-2 w-48 bg-white shadow-lg rounded-lg z-10">
+                        <button
+                            type="button"
+                            @click="logout()"
+                            class="block text-gray-700 px-4 py-2 transition duration-200 ease-in-out hover:bg-gray-100 w-full text-start">
+                            Logout
+                        </button>
+                    </div>
+                </Transition>
+            </div>
         </div>
         <slot />
     </div>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import Checkbox from "@/Components/Checkbox.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 
 defineProps({
     canResetPassword: {
@@ -17,52 +17,132 @@ defineProps({
 });
 
 const form = useForm({
-    np: '',
-    password: '',
+    np: "",
+    password: "",
     remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
     });
 };
 </script>
 
 <template>
-    <GuestLayout>
-
-        <Head title="Log in" />
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+    <div class="h-screen overflow-hidden">
+        <div class="h-full w-screen flex items-center bg-gradient-to-br from-slate-300 to-slate-500">
+            <div
+                class="flex flex-col w-full mx-auto p-8 md:p-10 2xl:p-12 3xl:p-14 bg-[#ffffff] rounded-2xl shadow-xl max-w-xl"
+            >
+                <div class="flex flex-row gap-3 pb-4">
+                    <!-- <div>
+                        <img src="/favicon.svg" width="50" alt="Logo" />
+                    </div> -->
+                    <h1
+                        class="text-3xl font-bold text-[#4B5563] text-[#4B5563]"
+                    >
+                        Label Generator
+                    </h1>
+                </div>
+                <div class="text-sm font-light text-[#6B7280] pb-8">
+                    Selamat datang, silahkan login untuk memulai.
+                </div>
+                <form @submit.prevent="submit" class="flex flex-col">
+                    <div class="pb-2">
+                        <label
+                            for="npUser"
+                            class="block mb-2 text-sm font-medium text-[#111827]"
+                            >Nomor Pegawai</label
+                        >
+                        <div class="relative text-gray-400">
+                            <span
+                                class="absolute inset-y-0 left-0 flex items-center p-1 pl-3"
+                                ><svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="lucide lucide-mail"
+                                >
+                                    <rect
+                                        width="20"
+                                        height="16"
+                                        x="2"
+                                        y="4"
+                                        rx="2"
+                                    ></rect>
+                                    <path
+                                        d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"
+                                    ></path></svg
+                            ></span>
+                            <input
+                                type="text"
+                                name="npUser"
+                                id="npUser"
+                                v-model="form.np"
+                                class="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4"
+                                placeholder="Nomor Pegawai"
+                                autocomplete="off"
+                            />
+                        </div>
+                    </div>
+                    <div class="pb-6">
+                        <label
+                            for="password"
+                            class="block mb-2 text-sm font-medium text-[#111827]"
+                            >Password</label
+                        >
+                        <div class="relative text-gray-400">
+                            <span
+                                class="absolute inset-y-0 left-0 flex items-center p-1 pl-3"
+                                ><svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="lucide lucide-square-asterisk"
+                                >
+                                    <rect
+                                        width="18"
+                                        height="18"
+                                        x="3"
+                                        y="3"
+                                        rx="2"
+                                    ></rect>
+                                    <path d="M12 8v8"></path>
+                                    <path d="m8.5 14 7-4"></path>
+                                    <path d="m8.5 10 7 4"></path></svg
+                            ></span>
+                            <input
+                                v-model="form.password"
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="••••••••••"
+                                class="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4"
+                                autocomplete="new-password"
+                            />
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        class="w-full text-[#FFFFFF] bg-[#4F46E5] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="np" value="NP" />
-
-                <TextInput id="np" type="text" class="mt-1 block w-full drop-shadow-sm shadow-none" v-model="form.np"
-                    required autofocus autocomplete="username" />
-
-                <InputError class="mt-2" :message="form.errors.np" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput id="password" type="password" class="mt-1 block w-full  drop-shadow-sm shadow-none"
-                    v-model="form.password" required autocomplete="current-password" />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+    </div>
 </template>
