6893123d907427d009a99a00e9deb73db4a7a43b
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-10-23 11:33:15 +0700

    fix link underline

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref } from "vue";
 import NavLink from "@/Components/NavLink.vue";
-import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/vue3";
 import GroupLink from "@/Components/GroupLink.vue";
 
@@ -9,12 +8,12 @@ const showingNavigationDropdown = ref(false);
 const showOrderBesarGroup = ref(false);
 const showOrderKecilGroup = ref(false);
 
-const toogleDropdown = (dropDownId) => {
-    if(dropDownId == 1){
-        showOrderBesarGroup.value = showOrderBesarGroup.value == false ? true : false;
+const toggleDropdown = (dropDownId) => {
+    if (dropDownId === 1) {
+        showOrderBesarGroup.value = !showOrderBesarGroup.value;
         showOrderKecilGroup.value = false;
-    } else if (dropDownId == 2) {
-        showOrderKecilGroup.value = showOrderKecilGroup.value == false ? true : false;
+    } else if (dropDownId === 2) {
+        showOrderKecilGroup.value = !showOrderKecilGroup.value;
         showOrderBesarGroup.value = false;
     }
 }
@@ -28,82 +27,80 @@ const toogleDropdown = (dropDownId) => {
             <!-- Navigation -->
             <div class="flex justify-start items-center gap-6">
                 <!-- Order Besar -->
-                <GroupLink @click="toogleDropdown(1)"
-                    :active="$page.url == route('orderBesar.poSiapVerif') || route('dashboard')"
-                    class="relative group">
-                    <div class="flex items-center">
-                        <span class="text-lg font-semibold text-gray-800">Order Besar</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 ml-1 inline-block">
+                <div class="relative">
+                    <div @click="toggleDropdown(1)" class="flex items-center cursor-pointer">
+                        <span class="text-sm font-semibold text-gray-800">Order Besar</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-1">
                             <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd" />
                         </svg>
                     </div>
-                    <Transition animation="slide-in-right">
-                        <div class="absolute flex flex-col gap-2 bg-white shadow-lg rounded-md -left-28 px-4 py-2 top-3 hidden group-hover:block">
-                            <NavLink @mouseover="showOrderBesarGroup = true"
-                                :href="route('orderBesar.poSiapVerif') || route('dashboard')"
-                                :active="$page.url == route('orderBesar.poSiapVerif') || route('dashboard')"
-                                class="text-gray-700 hover:bg-gray-100 rounded-md px-2 py-1">
+                    <Transition name="fade">
+                        <div v-show="showOrderBesarGroup" class="absolute left-0 mt-2 w-48 bg-white shadow-lg z-10">
+                            <NavLink
+                                :href="route('orderBesar.poSiapVerif')"
+                                :active="route().current('orderBesar.poSiapVerif')"
+                                class="block text-gray-700  px-4 py-2 transition duration-200 ease-in-out w-full">
                                 Order Siap Periksa
                             </NavLink>
-
-                            <NavLink @mouseover="showOrderBesarGroup = true"
+                            <NavLink
                                 :href="route('orderBesar.registerNomorPo')"
-                                :active="$page.url == route('orderBesar.registerNomorPo')"
-                                class="text-gray-700 hover:bg-gray-100 rounded-md px-2 py-1">
+                                :active="route().current('orderBesar.registerNomorPo')"
+                                class="block text-gray-700 px-4 py-2 transition duration-200 ease-in-out w-full">
                                 Register Nomor PO
                             </NavLink>
                         </div>
                     </Transition>
-                </GroupLink>
+                </div>
 
                 <!-- Order Kecil -->
-                <GroupLink @click="toogleDropdown(2)"
-                    :active="$page.url == route('orderKecil.cetakLabel')"
-                    class="relative group">
-                    <div class="flex items-center">
-                        <span class="text-lg font-semibold text-gray-800">Order Kecil</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 ml-1 inline-block">
+                <div class="relative">
+                    <div @click="toggleDropdown(2)" class="flex items-center cursor-pointer">
+                        <span class="text-sm font-semibold text-gray-800">Order Kecil</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-1">
                             <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd" />
                         </svg>
                     </div>
-                    <Transition animation="slide-in-right">
-                        <div class="absolute flex flex-col gap-2 bg-white shadow-lg rounded-md -left-28 px-4 py-2 top-3 hidden group-hover:block">
+                    <Transition name="fade">
+                        <div v-show="showOrderKecilGroup" class="absolute left-0 mt-2 w-48 bg-white shadow-lg z-10">
                             <NavLink
                                 :href="route('orderKecil.cetakLabel')"
-                                :active="$page.url == route('orderKecil.cetakLabel')"
-                                class="text-gray-700 hover:bg-gray-100 rounded-md px-2 py-1">
+                                :active="route().current('orderKecil.cetakLabel')"
+                                class="block text-gray-700  px-4 py-2 transition duration-200 ease-in-out w-full">
                                 Cetak Label
                             </NavLink>
                         </div>
                     </Transition>
-                </GroupLink>
+                </div>
 
                 <!-- Monitoring Verifikasi -->
                 <NavLink
                     :href="route('monitoringProduksi.statusVerif.index')"
-                    :active="$page.url == route('monitoringProduksi.statusVerif.index')"
-                    class="text-gray-800 hover:bg-gray-100 rounded-md px-2 py-1">
+                    :active="route().current('monitoringProduksi.statusVerif.index')"
+                    class="text-gray-800  px-2 py-1 transition duration-200 ease-in-out">
                     Monitoring Produksi
                 </NavLink>
 
                 <!-- Data Production Order -->
                 <NavLink
                     :href="route('dataPo.index',0)"
-                    class="text-gray-800 hover:bg-gray-100 rounded-md px-2 py-1">
+                    :active="route().current('dataPo.index')"
+                    class="text-gray-800  px-2 py-1 transition duration-200 ease-in-out">
                     Data PO
                 </NavLink>
 
                 <!-- Hasil Produksi Pegawai -->
                 <NavLink
                     :href="route('monitoringProduksi.produksiPegawai')"
-                    class="text-gray-800 hover:bg-gray-100 rounded-md px-2 py-1">
+                    :active="route().current('monitoringProduksi.produksiPegawai')"
+                    class="text-gray-800 px-2 py-1 transition duration-200 ease-in-out">
                     Produksi Pegawai
                 </NavLink>
 
                 <!-- Create User -->
                 <NavLink
                     :href="route('createUser.index')"
-                    class="text-gray-800 hover:bg-gray-100 rounded-md px-2 py-1">
+                    :active="route().current('createUser.index')"
+                    class="text-gray-800  px-2 py-1 transition duration-200 ease-in-out">
                     Create User
                 </NavLink>
             </div>
