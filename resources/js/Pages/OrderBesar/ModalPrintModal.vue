db8e37fc7bcc2ba2d82989fd38974347071f458f
ZulfikarHD <zulfikar.h9050@gmail.com>
2025-01-31 15:35:27 +0700

    split

---
+++ resources/js/Pages/OrderBesar/ModalPrintModal.vue
@@ -0,0 +1,11 @@
+<template>
+  <Modal :show="show" @close="onClose">
+    <form @submit.prevent="onSubmit" class="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-6">
+      <!-- Current print manual modal content -->
+    </form>
+  </Modal>
+</template>
+
+<script setup>
+// Extract print manual related logic here
+</script>
---
+++ resources/js/Pages/OrderBesar/ModalPrintUlang.vue
@@ -0,0 +1,11 @@
+<template>
+  <Modal :show="show" @close="onClose">
+    <form @submit.prevent="onSubmit" class="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-6">
+      <!-- Current print ulang modal content -->
+    </form>
+  </Modal>
+</template>
+
+<script setup>
+// Extract print ulang related logic here
+</script>
