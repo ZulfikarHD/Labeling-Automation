c6b041819554ca5dc1af3eb06bb481533372acd8
Zulfikar Hidayatullah <94290055+ZulfikarHD@users.noreply.github.com>
2025-02-01 11:32:11 +0700

    Delete resources/js/Pages/OrderBesar/FormLabel.vue

--- resources/js/Pages/OrderBesar/FormLabel.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <form @submit.prevent="onSubmit" class="space-y-8">
-    <!-- Current form content -->
-  </form>
-</template>
-
-<script setup>
-// Extract form related logic here
-</script>
