fd98d69b0f8768442b6ae434a0fb9e79e7dee934
ZulfikarHD <zulfikar.h9050@gmail.com>
2025-11-19 08:53:34 +0700

    Chore : Cleanup Unused Component

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -12,7 +12,7 @@ defineProps({
 <template>
     <Link
         :href="href"
-        class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+        class="group block w-full px-4 py-3 text-left text-sm font-medium leading-5 text-slate-700 dark:text-slate-300 hover:text-blue-600 dark:hover:text-blue-400 hover:bg-blue-50 dark:hover:bg-blue-900/20 focus:outline-none focus:bg-blue-50 dark:focus:bg-blue-900/20 focus:text-blue-600 dark:focus:text-blue-400 transition-all duration-200 ease-in-out first:rounded-t-lg last:rounded-b-lg"
     >
         <slot />
     </Link>
